de3c2e2adb9f51e768f7eb4ce2a5999be14619a8
IanGlencross <ian@glenx.net>
2015-02-17 13:11:34 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions create a matrix with a cached inverse
+## that can be used much like methods on a class.
+## 
 
-## Write a short comment describing this function
+## The makeCacheMatrix function takes a maxtrix as an input
+## and provides a method  for updating (set) and accessing (get)
+## both the matirix and it's inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(i) inv <<- i
+        getinv <- function() inv
+        list (set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        mat <- x$get()
+        inv <- solve(mat, ...)
+        x$setinv(inv)
+        inv
 }
